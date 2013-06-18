4b249c9ffeadfeaa0543a5a5da2aca1cb1d7c205
Kumar Appaiah <a.kumar@alumni.iitm.ac.in>
2013-06-17 23:19:19 -0400

    Normalize modes and add test

--- fiber.py
+++ fiber.py
@@ -52,6 +52,34 @@ class GHModes(ModeFamily):
     Graded-Index Multimode Fiber in Presence of Spatial- and
     Polarization-Mode Coupling* by Shemirani et al., IEEE/OSA Journal
     of Lightwave Technology, May 2009.
+
+    An examples:
+    >>> NA = 0.19
+    >>> wavelength = 1.55e-6;
+    >>> a = 31.25e-6/2;
+    >>> k0 = 2 * numpy.pi / wavelength
+    >>> n0 = 1.444;
+    >>> Csk0 = 0.0878 * pow(n0, 3)
+    >>> delta = 8000;
+    >>> n_core = n0;
+    >>> DELTA = 0.5 * pow((NA / n0), 2);
+    >>> sqrt = numpy.sqrt
+    >>> w = sqrt(sqrt(2) * a / (k0 * n0 * sqrt(DELTA)));
+    >>> EXTENTS = 30e-6
+    >>> STEP = 0.5e-6
+    >>> x = numpy.arange(-EXTENTS, EXTENTS, STEP)
+    >>> y = numpy.arange(-EXTENTS, EXTENTS, STEP)
+    >>> [XX, YY] = numpy.meshgrid(x, y)
+    >>> gh_modes = GHModes(w, XX, YY, theta = numpy.pi / 4)
+    >>> # gh_modes.plot_mode_pattern(2, 2)
+    >>> mode_pattern_1 = gh_modes.get_mode_pattern(0, 0)
+    >>> mode_pattern_2 = gh_modes.get_mode_pattern(1, 1)
+    >>> print "%.2f" % utils.overlap(mode_pattern_1, mode_pattern_1)
+    1.00
+    >>> print "%.2f" % utils.overlap(mode_pattern_2, mode_pattern_2)
+    1.00
+    >>> print "%.2f" % utils.overlap(mode_pattern_1, mode_pattern_2)
+    0.00
     """
     def get_mode_pattern(self, p, q):
         """
@@ -77,20 +105,17 @@ class GHModes(ModeFamily):
         R2 = numpy.square(XX - self.offset_x) + numpy.square(YY - self.offset_y)
 
         # Split the multiplication into steps
-        P1 = sqrt(2 / pi) / w / sqrt(pow(2, p + q) * factorial(p) * factorial(q))
+        P1 = sqrt(2 / pi) / w / sqrt(pow(2.0, p + q) * float(factorial(p)) * float(factorial(q)))
+        P1 = 1.0
         P2 = P1 * Hp(sqrt(2.0) * (XX * cos(theta) + YY * sin(theta)) / w)
         P3 = numpy.multiply(P2, Hq(sqrt(2.0) * (XX * -sin(theta) + YY * cos(theta)) / w))
-        return numpy.multiply(P3, exp(-R2 / w / w))
+        P4 = numpy.multiply(P3, exp(-R2 / w / w))
+        P5 = P4 / sqrt(numpy.sum(numpy.square(P4)))
+        return P5
 
     def plot_mode_pattern(self, p, q):
         """
-        Plot the array using a fiber with ``fiber_diameter`` as backdrop.
-
-        Parameters:
-        ``fec``: Fiber edge colour
-        ``ffc``: Fiber face colour
-        ``dec``: Device edge colour
-        ``dfc``: Device face colour
+        Plot the `p`, `q` mode pattern.
         """
         try:
             import matplotlib
@@ -107,23 +132,6 @@ class GHModes(ModeFamily):
         pyplot.show()
 
 if __name__ == "__main__":
-    NA = 0.19
-    wavelength = 1.55e-6;
-    a = 31.25e-6/2;
-    k0 = 2 * numpy.pi / wavelength
-    n0 = 1.444;
-    Csk0 = 0.0878 * pow(n0, 3)
-    delta = 8000;
-    n_core = n0;
-    DELTA = 0.5 * pow((NA / n0), 2);
-    sqrt = numpy.sqrt
-    w = sqrt(sqrt(2) * a / (k0 * n0 * sqrt(DELTA)));
-    EXTENTS = 30e-6
-    STEP = 0.5e-6
-    x = numpy.arange(-EXTENTS, EXTENTS, STEP)
-    y = numpy.arange(-EXTENTS, EXTENTS, STEP)
-    [XX, YY] = numpy.meshgrid(x, y)
-    gh_modes = GHModes(w, XX, YY)
-    gh_modes.plot_mode_pattern(0, 1)
     import doctest
+    import utils
     doctest.testmod()
