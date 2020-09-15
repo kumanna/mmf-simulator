6976a95e77292c7ea38b2682267fc7e043911d8c
Kumar Appaiah <a.kumar@alumni.iitm.ac.in>
2020-09-15 12:52:43 +0530

    Update for Python 3

--- utils.py
+++ utils.py
@@ -22,7 +22,7 @@ simulation phases. For example:
 >>> M1 = numpy.matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
 >>> M2 = numpy.matrix([[9, 8, 7], [6, 5, 4], [3, 2, 1]])*1j
 >>> res = overlap(M1, M2)
->>> print "%.6f+%.6f" % (res.real, res.imag)
+>>> print("%.6f+%.6f" % (res.real, res.imag))
 0.000000+-0.578947
 """
 
@@ -32,9 +32,9 @@ def hermite_poly(n):
     """
     Returns the n-th degree physicists' Hermite polynomial.
     >>> hermite_poly(0)
-    array([ 1.])
+    array([1.])
     >>> hermite_poly(1)
-    array([ 0.,  2.])
+    array([0., 2.])
     >>> hermite_poly(2)
     array([-2.,  0.,  4.])
     >>> hermite_poly(3)
@@ -57,7 +57,7 @@ def overlap(Er1, Er2):
     """
     if Er1.shape != Er2.shape:
         raise ValueError('Matrix shapes do not match for overlap!')
-        
+
     # First find the normalization for the matrices
     E1_mag = numpy.linalg.norm(Er1, ord='fro')
     E2_mag = numpy.linalg.norm(Er1, ord='fro')
@@ -69,10 +69,9 @@ def bandwidth_to_wavelength_range(maxf, L, steps = 10):
     This method converts a given bandwidth range to a wavelength range
     for use with the simulator.
     >>> bandwidth_to_wavelength_range(1e10, 1.55e-6)
-    array([  1.55008009e-06,   1.55006229e-06,   1.55004449e-06,
-             1.55002669e-06,   1.55000890e-06,   1.54999110e-06,
-             1.54997331e-06,   1.54995551e-06,   1.54993772e-06,
-             1.54991992e-06])
+    array([1.55008009e-06, 1.55006229e-06, 1.55004449e-06, 1.55002669e-06,
+           1.55000890e-06, 1.54999110e-06, 1.54997331e-06, 1.54995551e-06,
+           1.54993772e-06, 1.54991992e-06])
     """
     fc = 3e8 / L
     minf_net = fc - maxf
